018a6bbd796929475599f880230b4bb9ce1c5be8
Admin <verukacolt@gmail.com>
2024-07-04 21:03:13 +0300

    Fix select

--- app/db/postgres_db.py
+++ app/db/postgres_db.py
@@ -1,11 +1,11 @@
+from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import text
 
 
 async def check_postgres_connection(session: AsyncSession) -> bool:
     try:
-        result = await session.execute(text("SELECT 1"))
+        result = await session.execute(select(1))
         return result.scalar() == 1
     except SQLAlchemyError as error:
         return str(error)
