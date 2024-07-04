aac08b564e75e7d30e6baa4c86a44530f8ae7f1e
Admin <verukacolt@gmail.com>
2024-07-04 20:18:03 +0300

    Commit

--- app/db/postgres_db.py
+++ app/db/postgres_db.py
@@ -1,18 +1,17 @@
 import logging
-from app.db.database import engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import text
-
 
 logger = logging.getLogger(__name__)
 
 
-async def check_postgres_connection():
+async def check_postgres_connection(session: AsyncSession) -> bool:
     try:
         logger.info("Attempting to connect to PostgreSQL")
-        async with engine.connect() as conn:
-            result = await conn.execute(text("SELECT 1"))
-            logger.info("Successfully connected to PostgreSQL")
-            return result.scalar() == 1
-    except Exception as error:
+        result = await session.execute(text("SELECT 1"))
+        logger.info("Successfully connected to PostgreSQL")
+        return result.scalar() == 1
+    except SQLAlchemyError as error:
         logger.error("Error connecting to PostgreSQL: %s", error)
         return str(error)
