8f75cbeacf70ccb5013f4520dbc7823d0dcfd26e
Admin <verukacolt@gmail.com>
2024-07-04 15:44:46 +0300

    async_sessionmaker

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,12 @@
 import logging
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from app.core.config import settings
 
 logger = logging.getLogger(__name__)
 
 engine = create_async_engine(settings.sqlalchemy_database_url, echo=True, future=True)
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_session() -> AsyncSession:
