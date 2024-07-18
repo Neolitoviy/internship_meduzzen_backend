838d6af79b5d8b99d86bead96d77424ff54bcdc4
Admin <verukacolt@gmail.com>
2024-07-18 14:41:37 +0300

    add models be 11

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,10 @@
+from .answer import Answer
 from .base import Base, metadata
 from .company_invitation import CompanyInvitation
 from .company_member import CompanyMember
 from .company_request import CompanyRequest
+from .question import Question
+from .quiz import Quiz
 from .user import User
 from .company import Company
 
@@ -13,4 +16,7 @@ __all__ = [
     "CompanyInvitation",
     "CompanyMember",
     "CompanyRequest",
+    "Quiz",
+    "Question",
+    "Answer",
 ]
