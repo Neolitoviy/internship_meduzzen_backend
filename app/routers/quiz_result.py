f2a3889b4dd683b18a4b23d153c12983ae73f25a
Admin <verukacolt@gmail.com>
2024-07-20 08:45:14 +0300

    Add export of quiz data from Redis in JSON and CSV formats for Owners/Admins

--- app/routers/quiz_result.py
+++ app/routers/quiz_result.py
@@ -1,5 +1,6 @@
+import json
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from app.routers.dependencies import UOWDep, CurrentUserDep, QuizResultServiceDep
 from app.schemas.quiz_result import QuizResultResponse, QuizVoteRequest, UserQuizVote
 
@@ -59,3 +60,37 @@ async def get_quiz_votes_redis(
         quiz_result_service: QuizResultServiceDep
 ):
     return await quiz_result_service.get_quiz_votes_from_redis(uow, current_user.id, user_id, company_id, quiz_id)
+
+
+@router.get("/export_quiz_results/{company_id}/{quiz_id}/csv")
+async def export_quiz_results_to_csv(
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+        current_user: CurrentUserDep,
+        uow: UOWDep,
+        quiz_result_service: QuizResultServiceDep
+):
+    csv_data = await quiz_result_service.export_quiz_results_from_redis_to_csv(uow, current_user.id, user_id,
+                                                                                 company_id,
+                                                                                 quiz_id)
+    response = Response(content=csv_data, media_type="text/csv")
+    response.headers["Content-Disposition"] = f"attachment; filename=quiz_results_{company_id}_{quiz_id}.csv"
+    return response
+
+
+@router.get("/export_quiz_results/{company_id}/{quiz_id}/json", response_model=List[UserQuizVote])
+async def export_quiz_results_to_json(
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+        current_user: CurrentUserDep,
+        uow: UOWDep,
+        quiz_result_service: QuizResultServiceDep
+):
+    json_data = await quiz_result_service.export_quiz_results_from_redis_to_json(uow, current_user.id, user_id,
+                                                                                 company_id,
+                                                                                 quiz_id)
+    response = Response(content=json_data, media_type="application/json")
+    response.headers["Content-Disposition"] = f"attachment; filename=quiz_results_{company_id}_{quiz_id}.json"
+    return response
--- app/services/quiz_result.py
+++ app/services/quiz_result.py
@@ -1,4 +1,6 @@
+import csv
 import json
+from io import StringIO
 from typing import List
 from app.db.redis_db import get_redis_client
 from app.schemas.quiz_result import QuizResultResponse, QuizVoteRequest, UserQuizVote
@@ -130,3 +132,35 @@ class QuizResultService:
         if data:
             return json.loads(data)
         return {"status": "not found"}
+
+    @staticmethod
+    async def export_quiz_results_from_redis_to_csv(uow: IUnitOfWork, current_user_id: int, user_id: int,
+                                                    company_id: int, quiz_id: int) -> str:
+        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(uow, current_user_id, user_id, company_id,
+                                                                       quiz_id)
+        output = StringIO()
+        writer = csv.writer(output)
+        writer.writerow(
+            ['User ID', 'Company ID', 'Quiz ID', 'Question ID', 'Question Text', 'Answer Text', 'Is Correct',
+             'Timestamp'])
+
+        for vote in quiz_votes:
+            writer.writerow([
+                vote.user_id,
+                vote.company_id,
+                vote.quiz_id,
+                vote.question_id,
+                vote.question_text,
+                vote.answer_text,
+                vote.is_correct,
+                vote.timestamp
+            ])
+
+        return output.getvalue()
+
+    @staticmethod
+    async def export_quiz_results_from_redis_to_json(uow: IUnitOfWork, current_user_id: int, user_id: int,
+                                                     company_id: int, quiz_id: int) -> str:
+        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(uow, current_user_id, user_id, company_id,
+                                                                       quiz_id)
+        return json.dumps([vote.dict() for vote in quiz_votes])
