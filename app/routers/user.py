cd3cbcdee9434cacd95f014ebb193df2afab24cc
Admin <verukacolt@gmail.com>
2024-07-24 16:01:10 +0300

    Update status code for user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, status
 
 from app.core.logging_config import logging_config
 from app.routers.dependencies import CurrentUserDep, UOWDep, UserServiceDep
@@ -21,12 +21,12 @@ router = APIRouter(
 )
 
 
-@router.post("/", response_model=UserResponse)
+@router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserCreateInput, uow: UOWDep, user_service: UserServiceDep):
     return await user_service.create_user(uow, UserCreate.model_validate(user))
 
 
-@router.get("/", response_model=UserListResponse)
+@router.get("/", response_model=UserListResponse, status_code=status.HTTP_200_OK)
 async def get_users(
     request: Request,
     uow: UOWDep,
@@ -37,12 +37,12 @@ async def get_users(
     return await user_service.get_users(uow, skip, limit, str(request.url))
 
 
-@router.get("/{user_id}", response_model=UserResponse)
+@router.get("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def get_user(user_id: int, uow: UOWDep, user_service: UserServiceDep):
     return await user_service.get_user_by_id(uow, user_id)
 
 
-@router.put("/{user_id}", response_model=UserResponse)
+@router.put("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
 async def update_user(
     user_id: int,
     user: UserUpdateInput,
@@ -55,7 +55,9 @@ async def update_user(
     )
 
 
-@router.delete("/{user_id}", response_model=UserResponse)
+@router.delete(
+    "/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK
+)
 async def delete_user(
     user_id: int,
     uow: UOWDep,
