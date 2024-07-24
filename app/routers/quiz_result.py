7f6071ac7d43170fe89768a9c3071c273737cebe
Admin <verukacolt@gmail.com>
2024-07-25 02:42:34 +0300

    Add status code 201 for quiz result create router

--- app/routers/quiz_result.py
+++ app/routers/quiz_result.py
@@ -1,10 +1,7 @@
-from typing import Dict
-
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.routers.dependencies import CurrentUserDep, QuizResultServiceDep, UOWDep
 from app.schemas.quiz_result import QuizResultResponse, QuizVoteRequest
-from app.services.quiz_result import QuizResultService
 
 router = APIRouter(
     prefix="/quiz_result",
@@ -13,7 +10,9 @@ router = APIRouter(
 
 
 @router.post(
-    "/quiz_results/vote/{company_id}/{quiz_id}", response_model=QuizResultResponse
+    "/vote/{company_id}/{quiz_id}",
+    response_model=QuizResultResponse,
+    status_code=status.HTTP_201_CREATED,
 )
 async def quiz_vote(
     company_id: int,
@@ -28,14 +27,14 @@ async def quiz_vote(
     )
 
 
-@router.get("/quiz_results/average_score/user/{user_id}", response_model=float)
+@router.get("/average_score/user/{user_id}", response_model=float)
 async def get_user_average_score(
     user_id: int, uow: UOWDep, quiz_result_service: QuizResultServiceDep
 ):
     return await quiz_result_service.get_user_average_score(uow, user_id)
 
 
-@router.get("/quiz_results/average_score/company/{company_id}", response_model=float)
+@router.get("/average_score/company/{company_id}", response_model=float)
 async def get_company_average_score(
     company_id: int,
     uow: UOWDep,
