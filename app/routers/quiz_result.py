c873ef95f45131708078c8b684fc07c48f08c422
Admin <verukacolt@gmail.com>
2024-07-24 05:51:47 +0300

    use linters 4

--- app/routers/quiz_result.py
+++ app/routers/quiz_result.py
@@ -1,7 +1,9 @@
 import json
 from typing import List
+
 from fastapi import APIRouter, Response
-from app.routers.dependencies import UOWDep, CurrentUserDep, QuizResultServiceDep
+
+from app.routers.dependencies import CurrentUserDep, QuizResultServiceDep, UOWDep
 from app.schemas.quiz_result import QuizResultResponse, QuizVoteRequest, UserQuizVote
 
 router = APIRouter(
@@ -10,87 +12,112 @@ router = APIRouter(
 )
 
 
-@router.post("/quiz_results/vote/{company_id}/{quiz_id}", response_model=QuizResultResponse)
-async def quiz_vote(company_id: int, quiz_id: int, vote_data: QuizVoteRequest, uow: UOWDep,
-                    current_user: CurrentUserDep, quiz_result_service: QuizResultServiceDep):
-    return await quiz_result_service.quiz_vote(uow, company_id, quiz_id, vote_data, current_user.id)
+@router.post(
+    "/quiz_results/vote/{company_id}/{quiz_id}", response_model=QuizResultResponse
+)
+async def quiz_vote(
+    company_id: int,
+    quiz_id: int,
+    vote_data: QuizVoteRequest,
+    uow: UOWDep,
+    current_user: CurrentUserDep,
+    quiz_result_service: QuizResultServiceDep,
+):
+    return await quiz_result_service.quiz_vote(
+        uow, company_id, quiz_id, vote_data, current_user.id
+    )
 
 
 @router.get("/quiz_results/average_score/user/{user_id}", response_model=float)
 async def get_user_average_score(
-        user_id: int,
-        company_id: int,
-        current_user: CurrentUserDep,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep
+    user_id: int,
+    company_id: int,
+    current_user: CurrentUserDep,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
 ):
-    return await quiz_result_service.get_user_average_score(uow, user_id, company_id, current_user.id)
+    return await quiz_result_service.get_user_average_score(
+        uow, user_id, company_id, current_user.id
+    )
 
 
 @router.get("/quiz_results/average_score/company/{company_id}", response_model=float)
 async def get_company_average_score(
-        company_id: int,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep,
-        current_user: CurrentUserDep
+    company_id: int,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
+    current_user: CurrentUserDep,
 ):
-    return await quiz_result_service.get_company_average_score(uow, company_id, current_user.id)
+    return await quiz_result_service.get_company_average_score(
+        uow, company_id, current_user.id
+    )
 
 
 @router.get("/get_vote_redis")
 async def get_vote_redis(
-        user_id: int,
-        company_id: int,
-        quiz_id: int,
-        question_id: int,
-        current_user: CurrentUserDep,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep
+    user_id: int,
+    company_id: int,
+    quiz_id: int,
+    question_id: int,
+    current_user: CurrentUserDep,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
 ):
-    return await quiz_result_service.get_vote_redis(uow, current_user.id, user_id, company_id, quiz_id, question_id)
+    return await quiz_result_service.get_vote_redis(
+        uow, current_user.id, user_id, company_id, quiz_id, question_id
+    )
 
 
 @router.get("/get_quiz_votes_redis", response_model=List[UserQuizVote])
 async def get_quiz_votes_redis(
-        user_id: int,
-        company_id: int,
-        quiz_id: int,
-        current_user: CurrentUserDep,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep
+    user_id: int,
+    company_id: int,
+    quiz_id: int,
+    current_user: CurrentUserDep,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
 ):
-    return await quiz_result_service.get_quiz_votes_from_redis(uow, current_user.id, user_id, company_id, quiz_id)
+    return await quiz_result_service.get_quiz_votes_from_redis(
+        uow, current_user.id, user_id, company_id, quiz_id
+    )
 
 
 @router.get("/export_quiz_results/{company_id}/{quiz_id}/csv")
 async def export_quiz_results_to_csv(
-        user_id: int,
-        company_id: int,
-        quiz_id: int,
-        current_user: CurrentUserDep,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep
+    user_id: int,
+    company_id: int,
+    quiz_id: int,
+    current_user: CurrentUserDep,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
 ):
-    csv_data = await quiz_result_service.export_quiz_results_from_redis_to_csv(uow, current_user.id, user_id,
-                                                                                 company_id,
-                                                                                 quiz_id)
+    csv_data = await quiz_result_service.export_quiz_results_from_redis_to_csv(
+        uow, current_user.id, user_id, company_id, quiz_id
+    )
     response = Response(content=csv_data, media_type="text/csv")
-    response.headers["Content-Disposition"] = f"attachment; filename=quiz_results_{company_id}_{quiz_id}.csv"
+    response.headers["Content-Disposition"] = (
+        f"attachment; filename=quiz_results_{company_id}_{quiz_id}.csv"
+    )
     return response
 
 
-@router.get("/export_quiz_results/{company_id}/{quiz_id}/json", response_model=List[UserQuizVote])
+@router.get(
+    "/export_quiz_results/{company_id}/{quiz_id}/json",
+    response_model=List[UserQuizVote],
+)
 async def export_quiz_results_to_json(
-        user_id: int,
-        company_id: int,
-        quiz_id: int,
-        current_user: CurrentUserDep,
-        uow: UOWDep,
-        quiz_result_service: QuizResultServiceDep
+    user_id: int,
+    company_id: int,
+    quiz_id: int,
+    current_user: CurrentUserDep,
+    uow: UOWDep,
+    quiz_result_service: QuizResultServiceDep,
 ):
-    json_data = await quiz_result_service.export_quiz_results_from_redis_to_json(uow, current_user.id, user_id,
-                                                                                 company_id,
-                                                                                 quiz_id)
+    json_data = await quiz_result_service.export_quiz_results_from_redis_to_json(
+        uow, current_user.id, user_id, company_id, quiz_id
+    )
     response = Response(content=json_data, media_type="application/json")
-    response.headers["Content-Disposition"] = f"attachment; filename=quiz_results_{company_id}_{quiz_id}.json"
+    response.headers["Content-Disposition"] = (
+        f"attachment; filename=quiz_results_{company_id}_{quiz_id}.json"
+    )
     return response
--- app/services/quiz_result.py
+++ app/services/quiz_result.py
@@ -1,26 +1,39 @@
 import csv
 import json
+from datetime import datetime
 from io import StringIO
 from typing import List
+
+from app.core.exceptions import (
+    AnswerNotFound,
+    CompanyNotFound,
+    PermissionDenied,
+    QuestionNotFound,
+    QuizNotFound,
+)
 from app.db.redis_db import get_redis_client
 from app.schemas.quiz_result import QuizResultResponse, QuizVoteRequest, UserQuizVote
 from app.utils.unitofwork import IUnitOfWork
-from app.core.exceptions import QuizNotFound, PermissionDenied, QuestionNotFound, AnswerNotFound, CompanyNotFound
-from datetime import datetime
 
 
 class QuizResultService:
     @staticmethod
-    async def quiz_vote(uow: IUnitOfWork, company_id: int, quiz_id: int, vote_data: QuizVoteRequest,
-                        user_id: int) -> QuizResultResponse:
+    async def quiz_vote(
+        uow: IUnitOfWork,
+        company_id: int,
+        quiz_id: int,
+        vote_data: QuizVoteRequest,
+        user_id: int,
+    ) -> QuizResultResponse:
         async with uow:
             quiz = await uow.quizzes.find_one(id=quiz_id)
             if not quiz:
                 raise QuizNotFound("Quiz not found")
 
             company = await uow.companies.find_one(id=quiz.company_id)
-            if company.owner_id != user_id and not await uow.company_members.find_one(company_id=company_id,
-                                                                                      user_id=user_id):
+            if company.owner_id != user_id and not await uow.company_members.find_one(
+                company_id=company_id, user_id=user_id
+            ):
                 raise PermissionDenied("You do not have permission to take this quiz.")
 
             total_questions = 0
@@ -28,11 +41,15 @@ class QuizResultService:
             for question_id, answer_id in vote_data.answers.items():
                 question = await uow.questions.find_one(id=question_id)
                 if not question or question.quiz_id != quiz_id:
-                    raise QuestionNotFound(f"Question {question_id} not found in quiz {quiz_id}")
+                    raise QuestionNotFound(
+                        f"Question {question_id} not found in quiz {quiz_id}"
+                    )
 
                 answer = await uow.answers.find_one(id=answer_id)
                 if not answer or answer.question_id != question_id:
-                    raise AnswerNotFound(f"Answer {answer_id} not found for question {question_id}")
+                    raise AnswerNotFound(
+                        f"Answer {answer_id} not found for question {question_id}"
+                    )
 
                 is_correct = answer.is_correct
                 if is_correct:
@@ -46,39 +63,57 @@ class QuizResultService:
                     question_id=question_id,
                     question_text=question.question_text,
                     answer_text=answer.answer_text,
-                    is_correct=is_correct
+                    is_correct=is_correct,
                 )
                 await QuizResultService.save_quiz_vote_to_redis(vote)
 
             score = round((total_answers / total_questions) * 100, 2)
 
-            quiz_result = await uow.quiz_results.add_one({
-                'user_id': user_id,
-                'quiz_id': quiz_id,
-                'company_id': company_id,
-                'total_questions': total_questions,
-                'total_answers': total_answers,
-                'score': score,
-                'created_at': datetime.utcnow()
-            })
+            quiz_result = await uow.quiz_results.add_one(
+                {
+                    "user_id": user_id,
+                    "quiz_id": quiz_id,
+                    "company_id": company_id,
+                    "total_questions": total_questions,
+                    "total_answers": total_answers,
+                    "score": score,
+                    "created_at": datetime.utcnow(),
+                }
+            )
             return QuizResultResponse.model_validate(quiz_result)
 
     @staticmethod
-    async def get_user_average_score(uow: IUnitOfWork, user_id: int, company_id: int, current_user_id: int) -> float:
+    async def get_user_average_score(
+        uow: IUnitOfWork, user_id: int, company_id: int, current_user_id: int
+    ) -> float:
         async with uow:
             company = await uow.companies.find_one(id=company_id)
-            if company.owner_id != current_user_id and not await uow.company_members.find_one(company_id=company_id,
-                                                                                              user_id=current_user_id):
-                raise PermissionDenied("You do not have permission to view this user's average score.")
+            if (
+                company.owner_id != current_user_id
+                and not await uow.company_members.find_one(
+                    company_id=company_id, user_id=current_user_id
+                )
+            ):
+                raise PermissionDenied(
+                    "You do not have permission to view this user's average score."
+                )
             return await uow.quiz_results.get_average_score(user_id=user_id)
 
     @staticmethod
-    async def get_company_average_score(uow: IUnitOfWork, company_id: int, current_user_id: int) -> float:
+    async def get_company_average_score(
+        uow: IUnitOfWork, company_id: int, current_user_id: int
+    ) -> float:
         async with uow:
             company = await uow.companies.find_one(id=company_id)
-            if company.owner_id != current_user_id and not await uow.company_members.find_one(company_id=company_id,
-                                                                                              user_id=current_user_id):
-                raise PermissionDenied("You do not have permission to view this company's average score.")
+            if (
+                company.owner_id != current_user_id
+                and not await uow.company_members.find_one(
+                    company_id=company_id, user_id=current_user_id
+                )
+            ):
+                raise PermissionDenied(
+                    "You do not have permission to view this company's average score."
+                )
             return await uow.quiz_results.get_average_score(company_id=company_id)
 
     @staticmethod
@@ -89,24 +124,34 @@ class QuizResultService:
         await connection.setex(answer_key, 172800, json.dumps(vote.dict()))
 
     @staticmethod
-    async def get_quiz_votes_from_redis(uow: IUnitOfWork, current_user_id: int, user_id: int, company_id: int,
-                                        quiz_id: int) -> List[UserQuizVote]:
+    async def get_quiz_votes_from_redis(
+        uow: IUnitOfWork,
+        current_user_id: int,
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+    ) -> List[UserQuizVote]:
         async with uow:
             company = await uow.companies.find_one(id=company_id)
             if not company:
                 raise QuizNotFound("Company not found")
             if user_id != current_user_id:
-                if company.owner_id != current_user_id and not await uow.company_members.find_one(company_id=company_id,
-                                                                                                  user_id=current_user_id,
-                                                                                                  is_admin=True):
-                    raise PermissionDenied("You do not have permission to view this company's quiz votes.")
+                if (
+                    company.owner_id != current_user_id
+                    and not await uow.company_members.find_one(
+                        company_id=company_id, user_id=current_user_id, is_admin=True
+                    )
+                ):
+                    raise PermissionDenied(
+                        "You do not have permission to view this company's quiz votes."
+                    )
         connection = await get_redis_client()
         user_key_pattern = f"quiz_vote:{user_id}:{company_id}:{quiz_id}:*"
         user_keys = await connection.keys(user_key_pattern)
         quiz_votes = []
 
         for user_key in user_keys:
-            user_key_str = str(user_key)[len("<Future finished result='"):-2].strip()
+            user_key_str = str(user_key)[len("<Future finished result='") : -2].strip()
             answer_data_json = await connection.get(user_key_str)
             if answer_data_json:
                 answer_data = json.loads(answer_data_json)
@@ -115,17 +160,28 @@ class QuizResultService:
         return [UserQuizVote.model_validate(vote) for vote in quiz_votes]
 
     @staticmethod
-    async def get_vote_redis(uow: IUnitOfWork, current_user_id: int, user_id: int, company_id: int, quiz_id: int,
-                             question_id: int):
+    async def get_vote_redis(
+        uow: IUnitOfWork,
+        current_user_id: int,
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+        question_id: int,
+    ):
         async with uow:
             company = await uow.companies.find_one(id=company_id)
             if not company:
                 raise CompanyNotFound("Company not found")
             if user_id != current_user_id:
-                if company.owner_id != current_user_id and not await uow.company_members.find_one(company_id=company_id,
-                                                                                                  user_id=current_user_id,
-                                                                                                  is_admin=True):
-                    raise PermissionDenied("You do not have permission to view this company's quiz votes.")
+                if (
+                    company.owner_id != current_user_id
+                    and not await uow.company_members.find_one(
+                        company_id=company_id, user_id=current_user_id, is_admin=True
+                    )
+                ):
+                    raise PermissionDenied(
+                        "You do not have permission to view this company's quiz votes."
+                    )
         connection = await get_redis_client()
         key = f"quiz_vote:{user_id}:{company_id}:{quiz_id}:{question_id}"
         data = await connection.get(key)
@@ -134,33 +190,56 @@ class QuizResultService:
         return {"status": "not found"}
 
     @staticmethod
-    async def export_quiz_results_from_redis_to_csv(uow: IUnitOfWork, current_user_id: int, user_id: int,
-                                                    company_id: int, quiz_id: int) -> str:
-        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(uow, current_user_id, user_id, company_id,
-                                                                       quiz_id)
+    async def export_quiz_results_from_redis_to_csv(
+        uow: IUnitOfWork,
+        current_user_id: int,
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+    ) -> str:
+        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(
+            uow, current_user_id, user_id, company_id, quiz_id
+        )
         output = StringIO()
         writer = csv.writer(output)
         writer.writerow(
-            ['User ID', 'Company ID', 'Quiz ID', 'Question ID', 'Question Text', 'Answer Text', 'Is Correct',
-             'Timestamp'])
+            [
+                "User ID",
+                "Company ID",
+                "Quiz ID",
+                "Question ID",
+                "Question Text",
+                "Answer Text",
+                "Is Correct",
+                "Timestamp",
+            ]
+        )
 
         for vote in quiz_votes:
-            writer.writerow([
-                vote.user_id,
-                vote.company_id,
-                vote.quiz_id,
-                vote.question_id,
-                vote.question_text,
-                vote.answer_text,
-                vote.is_correct,
-                vote.timestamp
-            ])
+            writer.writerow(
+                [
+                    vote.user_id,
+                    vote.company_id,
+                    vote.quiz_id,
+                    vote.question_id,
+                    vote.question_text,
+                    vote.answer_text,
+                    vote.is_correct,
+                    vote.timestamp,
+                ]
+            )
 
         return output.getvalue()
 
     @staticmethod
-    async def export_quiz_results_from_redis_to_json(uow: IUnitOfWork, current_user_id: int, user_id: int,
-                                                     company_id: int, quiz_id: int) -> str:
-        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(uow, current_user_id, user_id, company_id,
-                                                                       quiz_id)
+    async def export_quiz_results_from_redis_to_json(
+        uow: IUnitOfWork,
+        current_user_id: int,
+        user_id: int,
+        company_id: int,
+        quiz_id: int,
+    ) -> str:
+        quiz_votes = await QuizResultService.get_quiz_votes_from_redis(
+            uow, current_user_id, user_id, company_id, quiz_id
+        )
         return json.dumps([vote.dict() for vote in quiz_votes])
